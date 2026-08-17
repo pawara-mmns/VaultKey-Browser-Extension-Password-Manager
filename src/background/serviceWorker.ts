7433ec6ffcfa2e7974af85cb41d14afc62ef840a
pawara-mmns <pawarasamarawickrama@gmail.com>
2026-08-17 09:26:53 +0530

    feat: enhance service worker with auto-lock and clipboard clear alarm handling

--- src/background/serviceWorker.ts
+++ src/background/serviceWorker.ts
@@ -1,10 +1,26 @@
 import { initializeStorageAccess } from "../storage/storageAccess";
-import { clearSession } from "../security/session";
+import { handleAutoLockAlarm, scheduleAutoLock } from "../services/activityService";
+import { handleClipboardClearAlarm, restoreClipboardAlarm } from "../services/clipboardService";
+import { AUTO_LOCK_ALARM, CLIPBOARD_CLEAR_ALARM } from "../services/runtimeConstants";
 
 // Keep initialization lightweight; unlocked key material lives in storage.session, not worker memory.
 void initializeStorageAccess();
+void scheduleAutoLock();
+void restoreClipboardAlarm();
 
 chrome.runtime.onInstalled.addListener(() => {
   void initializeStorageAccess();
-  void clearSession();
+  void scheduleAutoLock();
+  void restoreClipboardAlarm();
+});
+
+chrome.runtime.onStartup.addListener(() => {
+  void initializeStorageAccess();
+  void scheduleAutoLock();
+  void restoreClipboardAlarm();
+});
+
+chrome.alarms.onAlarm.addListener((alarm) => {
+  if (alarm.name === AUTO_LOCK_ALARM) void handleAutoLockAlarm();
+  if (alarm.name === CLIPBOARD_CLEAR_ALARM) void handleClipboardClearAlarm();
 });
